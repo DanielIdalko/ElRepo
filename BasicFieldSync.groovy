303021e7a6d2cb69d08f2a0cedb6d602cd99321e
DanielIdalko <80425585+DanielIdalko@users.noreply.github.com>
2023-08-10 15:10:04 -0600

    Update BasicFieldSync.groovy

--- BasicFieldSync.groovy
+++ BasicFieldSync.groovy
@@ -9,7 +9,7 @@ class BasicFieldSync
     issue.summary      = replica.summary
 	issue.description  = replica.description
 	issue.assignee     = nodeHelper.getUserByUsername(replica.assignee?.username)
-	issue.reporter     = nodeHelper.getUserByUsername(replica.reporter?.username)
+	//issue.reporter     = nodeHelper.getUserByUsername(replica.reporter?.username)
 	issue.labels       = replica.labels
 	issue.comments     = commentHelper.mergeComments(issue, replica)
 	issue.attachments  = attachmentHelper.mergeAttachments(issue, replica)

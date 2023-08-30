17a8bba5731527138ccb4249ee71e50159f69932
DanielIdalko <80425585+DanielIdalko@users.noreply.github.com>
2023-08-30 07:58:57 -0600

    Update BasicFieldSync.groovy

--- BasicFieldSync.groovy
+++ BasicFieldSync.groovy
@@ -3,15 +3,15 @@ class BasicFieldSync
   static receive(issue,
 		 		 replica,
 		 		 nodeHelper,
-		 		 commentHelper,
-		 		 attachmentHelper) {
+		 		 commentHelper
+		 		 ) {
 
-    issue.summary      = replica.summary
+    	issue.summary      = replica.summary
 	issue.description  = replica.description
 	issue.assignee     = nodeHelper.getUserByUsername(replica.assignee?.username)
 	issue.reporter     = nodeHelper.getUserByUsername(replica.reporter?.username)
 	issue.labels       = replica.labels
 	issue.comments     = commentHelper.mergeComments(issue, replica)
-	issue.attachments  = attachmentHelper.mergeAttachments(issue, replica)
+	//issue.attachments  = attachmentHelper.mergeAttachments(issue, replica)
   }
 }

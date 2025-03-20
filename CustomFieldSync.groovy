9a372619868ffbf069a5a3785f527886dfbb5438
DanielIdalko <80425585+DanielIdalko@users.noreply.github.com>
2025-03-20 10:24:10 -0600

    Update CustomFieldSync.groovy

--- CustomFieldSync.groovy
+++ CustomFieldSync.groovy
@@ -4,6 +4,7 @@ class CustomFieldSync {
   	issue.summary      = replica.summary
 	issue.description  = replica.description
 	issue.comments = commentHelper.mergeComments(issue, replica)
+	debug.log("repoIsTested")
 	
   }
 }
